f712fc1547510b2f1029ff0234cadc187e9e275d
Tilottama Shinde <162155750+TilottamaShinde@users.noreply.github.com>
2025-07-21 23:16:43 +0530

    Update parse_resume.py

--- parse_resume.py
+++ parse_resume.py
@@ -18,6 +18,6 @@ def extract_text_from_pdf(pdf_path: str) -> str:
     text = ""
     with pdfplumber.open(pdf_path) as pdf:
         for page in pdf.pages:
-            text += page.extract_text() and ""
+            text += page.extract_text() or ""
 
     return text.strip()
