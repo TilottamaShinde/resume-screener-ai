725b3483ad61b54f7d2067dc16b943ab74c993f8
Tilottama Shinde <162155750+TilottamaShinde@users.noreply.github.com>
2025-07-26 21:38:09 +0530

    Update export_results.py

--- export_results.py
+++ export_results.py
@@ -1,1 +1,10 @@
 import csv
+
+def export_to_csv(results, output_file = 'screening_results.csv'):
+   """
+    Exports screening results to a CSV file.
+
+    Parameters:
+        results (list of dict): Each dict contains 'filename', 'score', 'keywords'
+        output_file (str): The name of the output CSV file
+    """
